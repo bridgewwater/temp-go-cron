82c3f0f8e4cac9dbcb1961b0d5e4e5368b8b15c2
sinlov <sinlovppt@gmail.com>
2020-03-16 17:46:13 +0800

    change MakeGoMod.mk to support linux

--- MakeGoMod.mk
+++ MakeGoMod.mk
@@ -2,31 +2,19 @@
 
 modVerify:
 	# in GOPATH must use [ GO111MODULE=on go mod ] to use
-	-if [[ $(ENV_NEED_PROXY) -eq 1 ]]; \
-	then GOPROXY="$(ENV_GO_PROXY)" GO111MODULE=on go mod verify; \
-	else GO111MODULE=on go mod verify; \
-	fi
+	-if [[ $(ENV_NEED_PROXY) -eq 1 ]]; then GOPROXY="$(ENV_GO_PROXY)" GO111MODULE=on go mod verify; else GO111MODULE=on go mod verify; fi
 
 modDownload:
-	-if [[ $(ENV_NEED_PROXY) -eq 1 ]]; \
-	then GOPROXY="$(ENV_GO_PROXY)" GO111MODULE=on go mod download && GOPROXY="$(ENV_GO_PROXY)" GO111MODULE=on go mod vendor; \
-	else GO111MODULE=on go mod download && GO111MODULE=on go mod vendor; \
-	fi
+	-if [[ $(ENV_NEED_PROXY) -eq 1 ]]; then GOPROXY="$(ENV_GO_PROXY)" GO111MODULE=on go mod download && GOPROXY="$(ENV_GO_PROXY)" GO111MODULE=on go mod vendor; else GO111MODULE=on go mod download && GO111MODULE=on go mod vendor; fi
 
 modTidy:
-	-if [[ $(ENV_NEED_PROXY) -eq 1 ]]; \
-	then GOPROXY="$(ENV_GO_PROXY)" GO111MODULE=on go mod tidy; \
-	else GO111MODULE=on go mod tidy; \
-	fi
+	-if [[ $(ENV_NEED_PROXY) -eq 1 ]]; then GOPROXY="$(ENV_GO_PROXY)" GO111MODULE=on go mod tidy; else GO111MODULE=on go mod tidy; fi
 
 dep: modVerify modDownload
 	@echo "just check depends info below"
 
 modGraphDependencies:
-	-if [[ $(ENV_NEED_PROXY) -eq 1 ]]; \
-	then GOPROXY="$(ENV_GO_PROXY)" GO111MODULE=on go mod graph; \
-	else GO111MODULE=on go mod graph; \
-	fi
+	-if [[ $(ENV_NEED_PROXY) -eq 1 ]]; then GOPROXY="$(ENV_GO_PROXY)" GO111MODULE=on go mod graph; else GO111MODULE=on go mod graph; fi
 
 helpGoMod:
 	@echo "Help: MakeGoMod.mk"
